0beb4f37015e0b15a5388c3c8497e2034c3c13aa
Etch <78042695+Etch9@users.noreply.github.com>
2024-08-18 13:56:44 +0200

    Refactor out Conversion code and clean up of conversion calculations

---
+++ Assets/M1_to_M2_converter/Editor/MG_MG2Converter.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MagicaCapsuleCollider1 = MagicaCloth.MagicaCapsuleCollider;
+using Magica1Axis = MagicaCloth.MagicaCapsuleCollider.Axis;
+using Magica2Axis = MagicaCloth2.MagicaCapsuleCollider.Direction;
+using MagicaCapsuleCollider2 = MagicaCloth2.MagicaCapsuleCollider;
+using MagicaPlaneCollider1 = MagicaCloth.MagicaPlaneCollider;
+using MagicaPlaneCollider2 = MagicaCloth2.MagicaPlaneCollider;
+
+namespace MagicaConverter{
+	public class MG_MG2Converter
+	{
+
+		public static void convertCollider(MagicaCapsuleCollider1 mg1_collider, MagicaCapsuleCollider2 newCollider){
+			float length= mg1_collider.Length;
+			float startRadius = mg1_collider.StartRadius;
+			float endRadius = mg1_collider.EndRadius;
+			float average = (startRadius+endRadius)/2f;
+			float centerMovement =0;
+
+			if(startRadius>endRadius){
+				centerMovement = -(startRadius-average);
+			}else if(endRadius>startRadius){
+				centerMovement = endRadius - average;
+			}
+			
+			//radius is actually diameter, MG2 handles the radius differently.
+			//In MG2 the lenght is fixed at the edge of the sphere and grows from the edge to the center point of the collider.
+			//In MG the lenght is fixes at the center of the sphere and grows form the center out.
+			//MG2 also doubles the lenght
+			float newLength = (length+startRadius/2f+endRadius/2f)*2;
+			Debug.Log($"NEW:{newLength}");
+			if(newLength>2){
+				Debug.LogWarning("Collider too long value will be clamped");
+			}
+
+			Magica1Axis axis = mg1_collider.AxisMode;
+			newCollider.radiusSeparation = true;
+			newCollider.SetSize(endRadius,startRadius,Mathf.Clamp(newLength,0f,2f));
+			newCollider.center = new Vector3(mg1_collider.Center.x,mg1_collider.Center.y,mg1_collider.Center.z);
+
+			//Because of what written earlier changing the radius in MG moved the center point of the collider depending on the axis
+			switch (axis)
+			{
+				case Magica1Axis.X:
+					newCollider.center.x += centerMovement;
+					newCollider.direction = Magica2Axis.X;
+					break;
+				case Magica1Axis.Y:
+					newCollider.center.y += centerMovement;
+					newCollider.direction = Magica2Axis.Y;
+					break;
+				case Magica1Axis.Z:
+					newCollider.center.z += centerMovement;					
+					newCollider.direction = Magica2Axis.Z;
+					break;
+				default:
+					Debug.LogError("Error converting axis, defaulting to X");
+					newCollider.center.x += centerMovement;
+					newCollider.direction = Magica2Axis.X;
+					break;
+			}
+		}
+
+		public static void convertCollider(MagicaPlaneCollider1 mg1_collider, MagicaPlaneCollider2 newCollider){
+
+		}
+	}
+}
+
--- Assets/M1_to_M2_converter/Editor/MagicaConverterWindow.cs
+++ Assets/M1_to_M2_converter/Editor/MagicaConverterWindow.cs
@@ -7,129 +7,121 @@ using Magica1Axis = MagicaCloth.MagicaCapsuleCollider.Axis;
 using Magica2Axis = MagicaCloth2.MagicaCapsuleCollider.Direction;
 using MagicaCapsuleCollider2 = MagicaCloth2.MagicaCapsuleCollider;
 
-public class MagicaConverterWindow : EditorWindow
-{
-	private GameObject selectedObject;
-	private Label selectedGameObjectLabel;
-    [MenuItem("Tools/MagicaConverter")]
-    public static void ShowWindow()
-    {
-        // Create or show the custom editor window
-        MagicaConverterWindow wnd = GetWindow<MagicaConverterWindow>();
-        wnd.titleContent = new GUIContent("MagicaConverter");
-    }
 
-    public void CreateGUI()
-    {
-        // Get the root visual element of the window
-        VisualElement root = rootVisualElement;
-
-		Label warningLabel = new Label("The conversion is not 100% accurate\n when using different start and end radius.\n Sometime the resulting lenght is too high,\n in such cases it will be clamped to 2");
-        
-        // Apply styles to the label
-        warningLabel.style.color = Color.yellow;
-        warningLabel.style.unityFontStyleAndWeight = FontStyle.Bold;
-        warningLabel.style.backgroundColor = new Color(1, 0, 0, 0.3f); // Semi-transparent red background
-        warningLabel.style.paddingLeft = 5;
-        warningLabel.style.paddingRight = 5;
-        warningLabel.style.paddingTop = 3;
-        warningLabel.style.paddingBottom = 3;
-        warningLabel.style.marginBottom = 10;
-
-        // Add the label to the root visual element
-        root.Add(warningLabel);
-        // Create an ObjectField for GameObject
-        ObjectField gameObjectField = new ObjectField("Select GameObject")
-        {
-            objectType = typeof(GameObject),  // Specify that we want to select GameObjects
-            allowSceneObjects = true          // Allow selection of scene objects
-        };
-
-        // Add the ObjectField to the root visual element
-        root.Add(gameObjectField);
-
-        // Create a label to display the selected GameObject
-        selectedGameObjectLabel = new Label("Selected GameObject: None");
-        root.Add(selectedGameObjectLabel);
-
-        // Update the label when a GameObject is selected
-        gameObjectField.RegisterValueChangedCallback(evt =>
-        {
-            selectedObject = evt.newValue as GameObject;
-        });
-
-		Button myButton = new Button()
-        {
-            text = "Convert!"
-        };
-
-		root.Add(myButton);
-        // Register a click event handler for the button
-        myButton.clicked += OnButtonClick;
-    }
-
-	public void Update(){
-		if(selectedObject != null){
-			string str = "\n";
-			MagicaCapsuleCollider1[] components = selectedObject.GetComponentsInChildren<MagicaCapsuleCollider1>(true);
-			foreach(MagicaCapsuleCollider1 component in components){
-				str += component.gameObject.name + " | " + component.GetType().Name;
-				str+= "\n";
-			}
-			selectedGameObjectLabel.text =  $"Selected GameObject: {str}";
-		}else {
-			selectedGameObjectLabel.text = "Selected GameObject: None";
+namespace MagicaConverter{
+	public class MagicaConverterWindow : EditorWindow
+	{
+		private enum ConversionTypes{
+			None,
+			Full,
+			Colliders,
+			Cloth,
+			Bone
 		}
-	}
 
-	private void OnButtonClick(){
-		MagicaCapsuleCollider1[] components = selectedObject.GetComponentsInChildren<MagicaCapsuleCollider1>(true);
-			foreach(MagicaCapsuleCollider1 component in components){
-				GameObject parentObject = component.gameObject;
-				float length= component.Length;
-				float startRadius = component.StartRadius;
-				float endRadius = component.EndRadius;
-				float average = (startRadius+endRadius)/2f;
-				float newLength = (length>0.015f)? (length+average)*2f : length*2f;
-				if(newLength>2){
-					Debug.LogWarning("Collider too long value will be clamped");
+		
+
+		private bool PRESERVE_COMPONENTS = false;
+		private GameObject selectedObject;
+		private Label selectedGameObjectLabel;
+		private ConversionTypes selectedOption = ConversionTypes.None;
+		[MenuItem("Tools/MagicaConverter")]
+		public static void ShowWindow()
+		{
+			// Create or show the custom editor window
+			MagicaConverterWindow wnd = GetWindow<MagicaConverterWindow>();
+			wnd.titleContent = new GUIContent("MagicaConverter");
+		}
+
+		public void CreateGUI()
+		{
+			// Get the root visual element of the window
+			VisualElement root = rootVisualElement;
+
+			Label warningLabel = new Label("The conversion is not 100% accurate\n when using different start and end radius.\n Sometime the resulting lenght is too high,\n in such cases it will be clamped to 2");
+			
+			// Apply styles to the label
+			warningLabel.style.color = Color.yellow;
+			warningLabel.style.unityFontStyleAndWeight = FontStyle.Bold;
+			warningLabel.style.backgroundColor = new Color(1, 0, 0, 0.3f); // Semi-transparent red background
+			warningLabel.style.paddingLeft = 5;
+			warningLabel.style.paddingRight = 5;
+			warningLabel.style.paddingTop = 3;
+			warningLabel.style.paddingBottom = 3;
+			warningLabel.style.marginBottom = 10;
+
+			// Add the label to the root visual element
+			root.Add(warningLabel);
+
+			EnumField enumField = new EnumField("Convertion Type", selectedOption);
+			enumField.Init(selectedOption); // Initialize the EnumField with the current value
+
+			// Handle the value change
+			enumField.RegisterValueChangedCallback(evt =>
+			{
+				selectedOption = (ConversionTypes)evt.newValue;
+				//Debug.Log("Selected option: " + selectedOption);
+			});
+
+			root.Add(enumField);
+
+			// Create an ObjectField for GameObject
+			ObjectField gameObjectField = new ObjectField("Select GameObject")
+			{
+				objectType = typeof(GameObject),  // Specify that we want to select GameObjects
+				allowSceneObjects = true          // Allow selection of scene objects
+			};
+
+			// Add the ObjectField to the root visual element
+			root.Add(gameObjectField);
+
+			// Create a label to display the selected GameObject
+			selectedGameObjectLabel = new Label("Selected GameObject: None");
+			root.Add(selectedGameObjectLabel);
+
+			// Update the label when a GameObject is selected
+			gameObjectField.RegisterValueChangedCallback(evt =>
+			{
+				selectedObject = evt.newValue as GameObject;
+			});
+
+			Button myButton = new Button()
+			{
+				text = "Convert!"
+			};
+
+			root.Add(myButton);
+			// Register a click event handler for the button
+			myButton.clicked += convert;
+		}
+
+		public void Update(){
+			if(selectedObject != null){
+				string str = "\n";
+				MagicaCapsuleCollider1[] components = selectedObject.GetComponentsInChildren<MagicaCapsuleCollider1>(true);
+				foreach(MagicaCapsuleCollider1 component in components){
+					str += component.gameObject.name + " | " + component.GetType().Name;
+					str+= "\n";
 				}
-				Magica1Axis axis = component.AxisMode;
-				MagicaCapsuleCollider2 addedCollider = parentObject.AddComponent<MagicaCapsuleCollider2>();
-				addedCollider.radiusSeparation = true;
-				addedCollider.SetSize(endRadius,startRadius,Mathf.Clamp(newLength,0f,2f));
-				addedCollider.center = new Vector3(component.Center.x,component.Center.y,component.Center.z);
-				switch (axis)
-				{
-					case Magica1Axis.X:
-						if(startRadius>endRadius && length > (startRadius-average)){
-							addedCollider.center.x += -(startRadius-average);
-						}else if(endRadius>startRadius && length > (endRadius - average) ){
-							addedCollider.center.x += endRadius - average;
-						}
-						addedCollider.direction = Magica2Axis.X;
-						break;
-					case Magica1Axis.Y:
-						if(startRadius>endRadius && length > (startRadius-average)){
-							addedCollider.center.y += -(startRadius-average);
-						}else if(endRadius>startRadius && length > (endRadius - average)){
-							addedCollider.center.y += endRadius - average;
-						}
-						addedCollider.direction = Magica2Axis.Y;
-						break;
-					case Magica1Axis.Z:
-						if(startRadius>endRadius && length > (startRadius-average)){
-							addedCollider.center.z += -(startRadius-average);
-						}else if(endRadius>startRadius && length > (endRadius - average)){
-							addedCollider.center.z += endRadius - average;
-						}					
-						addedCollider.direction = Magica2Axis.Z;
-						break;
-					default:
-						Debug.LogError("Error converting axis, defaulting to X");
-						addedCollider.direction = Magica2Axis.X;
-						break;
-				}
+				selectedGameObjectLabel.text =  $"Selected GameObject: {str}";
+			}else {
+				selectedGameObjectLabel.text = "Selected GameObject: None";
 			}
+		}
+		private void convert(){
+			if(selectedOption == ConversionTypes.Colliders || selectedOption == ConversionTypes.Full){
+				Debug.Log($"selectedOption == ConversionTypes.Colliders | {selectedOption == ConversionTypes.Colliders}");
+				MG1_MG2_collider();
+			}
+		}
+		private void MG1_MG2_collider(){
+			MagicaCapsuleCollider1[] colliders = selectedObject.GetComponentsInChildren<MagicaCapsuleCollider1>(true);
+			foreach(MagicaCapsuleCollider1 collider in colliders){
+				GameObject parentObject = collider.gameObject;
+				
+				MagicaCapsuleCollider2 addedCollider = parentObject.AddComponent<MagicaCapsuleCollider2>();
+				MG_MG2Converter.convertCollider(collider,addedCollider);
+			}
+		}
 	}
 }
